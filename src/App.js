876805bde38842f0a6b9c9cc0114ac71dfc44005
Jigarsk <jigarsk30@gmail.com>
2024-07-03 17:18:09 +0530

    Final Commit 03-07-24

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-
+import { useEffect, useState } from "react";
 import './App.css';
+import Moviecard from "./MovieCard";
 
-const  App = () => {
-  return (
-   <>
-   <div className="App">
+const API_URL = 'http://www.omdbapi.com?apikey=f7950f80';
 
-  <h1>hello world</h1>
-   </div>
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
-   </>
-  );
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+        console.log(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies("marvel");
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Movies Hub</h1>
+            <div className="search">
+                <input
+                    placeholder="Search For Movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img
+                    src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+            {
+                movies?.length > 0 ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <Moviecard key={movie.imdbID} movie={movie} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No Movies Found</h2>
+                    </div>
+                )
+            }
+        </div>
+    );
 }
 
 export default App;
